9efe5552076efbab2865ff0a99e6fac8b46d4d8d
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2026-02-07 01:55:14 +0400

    feat: add TypeORM PostgreSQL configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+} from './env.config';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  synchronize: true,
+  autoLoadEntities: true,
+};
