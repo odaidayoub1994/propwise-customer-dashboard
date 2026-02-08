51d47845aa790cd7eb31776936baac735b44661f
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2026-02-08 20:30:01 +0400

    revert: restore toast position to top-right

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -17,7 +17,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <TooltipProvider>
           <AdminProvider>
             {children}
-            <Toaster richColors position="bottom-right" />
+            <Toaster richColors position="top-right" />
           </AdminProvider>
         </TooltipProvider>
       </ThemeProvider>
